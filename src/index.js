ff87f77c07db42126512befb744c0874162556fc
Breaver007 <96075427+Breaver007@users.noreply.github.com>
2022-02-20 21:34:06 +0300

    feat: task towe v2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    return matrix.flat(Infinity);
+    return matrix.reduce((acc, cur, i) => {
+        cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
+        return acc;
+    }, []);
 }
